5deb65da47b72939f87b79c3fdaa514400b39d39
Jonathan Liwanag <jonathanulandayliwanag@gmail.com>
2021-08-30 11:08:35 +0800

    Fix spacing and favicon udpate

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -4,17 +4,17 @@
     import Services from '$lib/services.svelte'
 </script>
 
-<header class="bg-[#020202] w-full px-24 py-8 flex items-center justify-between">
+<header class="bg-[#020202] w-full px-8 md:px-24 py-8 flex items-center justify-between">
     <a href="/">
         <img src="logo.png" alt="Skypiea Media Company" class="w-12 cursor-pointer hover:animate-spin transition-all duration-700">
     </a>
-    <nav class="text-white font-black space-x-12">
+    <nav class="text-white font-black space-x-6 md:space-x-12 flex">
         <a href="#" class="hover:text-blue-300 transition-colors duration-500">Our Work</a>
         <a href="#" class="hover:text-blue-300 transition-colors duration-500">Contact Us</a>
     </nav>
 </header>
 <slot></slot>
 <Services />
-<footer class="bg-[#020202] w-full px-24 py-8">
+<footer class="bg-[#020202] w-full md:px-24 py-8">
     <p class="text-white text-sm text-center tracking-wide">Copyright &copy; Skypiea Media Group</p>
 </footer>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -15,10 +15,10 @@
 
 <main class="h-full font-visby bg-[#151515]">
     <section class="h-2/4 m-auto flex flex-col space-y-6 justify-center items-center bg-[#0c0c0c] py-40 px-[10%]">
-        <h1 class="text-7xl text-center font-extrabold uppercase text-transparent bg-clip-text bg-gradient-to-br from-[#b97112] to-[#f8ba14]">Skypiea Media</h1>
+        <h1 class="text-4xl md:text-7xl text-center font-extrabold uppercase text-transparent bg-clip-text bg-gradient-to-br from-[#b97112] to-[#f8ba14]">Skypiea Media</h1>
         <p class="text-2xl text-center font-bold text-transparent bg-clip-text bg-gradient-to-br from-white to-gray-50">We help company build a solid foundation for their success through design.</p>
     </section>
-    <section class="flex bg-[#0e0e0e] flex-col items-center h-full w-full p-24">
+    <section class="flex bg-[#0e0e0e] flex-col items-center h-full w-full p-8 md:p-24">
         <h2 class="text-5xl font-bold mb-12 text-white">Our Portfolio</h2>
         <div class="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-5 gap-10 w-full">
             {#each works as work, i}
